990981e69efa987daeba25f7ab08d123d9b546cf
Undecember <ehdtjskaqhd@gmail.com>
2023-10-06 16:33:23 +0900

    Update to use qrcode

--- run.py
+++ run.py
@@ -19,21 +19,9 @@ browser.implicitly_wait(0)
 browser.set_window_size(1920, 1080)
 
 browser.get('https://nid.naver.com/nidlogin.login')
-browser.find_element(By.NAME, 'id').send_keys(username)
-browser.find_element(By.NAME, 'pw').send_keys(password)
-browser.find_element(By.ID, 'log.login').click()
-time.sleep(2)
-while True:
-    browser.save_screenshot('cim.png')
-    cap = input('captcha image saved in cim.png. Enter the answer : ').replace('\n', '')
-    if cap == 'done': break
-    browser.find_element(By.NAME, 'pw').send_keys(password)
-    browser.find_element(By.ID, 'captcha').send_keys(cap)
-    browser.find_element(By.ID, 'log.login').click()
-    time.sleep(2)
-browser.find_element(By.ID, 'new.dontsave').click()
-time.sleep(2)
-browser.get('https://cafe.naver.com/steamindiegame')
+browser.find_element(By.ID, 'qrcode').click()
+browser.save_screenshot('cim.png')
+input('Press enter after done : ')
 time.sleep(5)
 while True:
     while True:
